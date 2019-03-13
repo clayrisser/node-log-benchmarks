e1572226058034be051e3719db0f453490bf45ac
Jam Risser <jam@codejam.ninja>
2019-03-13 04:19:40 -0500

    Call benchmark from cli

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import WinstonBenchmark from './winston';
+import _ from 'lodash';
 
-const winstonBenchmark = new WinstonBenchmark({
-  iterations: 1000000,
-  appmetrics: true
+const { argv } = process;
+const name = argv[2];
+const benchmarkName = _.camelCase(
+  name.substr(0, name.indexOf(':')) || 'winston'
+);
+const metricsName = _.camelCase(
+  name.substr(name.indexOf(':'), name.length - 1) || 'console'
+);
+const appmetrics = _.includes(argv, '--appmetrics');
+const iterations = Number(argv[3] || 1000);
+const logger = console;
+const Benchmark = require(`./${benchmarkName}`).default;
+const benchmark = new Benchmark({
+  appmetrics,
+  iterations
 });
 
 async function main() {
   let result = {};
   process.on('exit', () => {
     const asyncEndTime = Date.now();
-    console.log({
+    result = {
       ...result,
       asyncEndTime,
       asyncTime: asyncEndTime - result.startTime
-    });
+    };
+    logger.log(result);
   });
-  result = await winstonBenchmark.filesystem();
+  result = await benchmark[metricsName]();
 }
 
 main();
