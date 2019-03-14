c2a3488a0beb396f6164319c559f3ecd241aba9d
Jam Risser <jam@codejam.ninja>
2019-03-14 19:12:33 +0530

    Added filesystem script in bash file

--- benchmark.sh
+++ benchmark.sh
@@ -8,6 +8,15 @@ npm run start -- log4js:console $ITERATIONS
 npm run start -- log4js:console $ITERATIONS --appmetrics
 npm run start -- bunyan:console $ITERATIONS
 npm run start -- bunyan:console $ITERATIONS --appmetrics
+
+npm run start -- winston:filesystem $ITERATIONS
+npm run start -- winston:filesystem $ITERATIONS --appmetrics
+npm run start -- log4js:filesystem $ITERATIONS
+npm run start -- log4js:filesystem $ITERATIONS --appmetrics
+npm run start -- bunyan:filesystem $ITERATIONS
+npm run start -- bunyan:filesystem $ITERATIONS --appmetrics
+
+
 # npm run json2csv -- winston.console.$ITERATIONS.json
 # npm run json2csv -- winston.console.$ITERATIONS.appmetrics.json
 
